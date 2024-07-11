f10c1d0e4b64b5b088d2c8aaf0e2d4d53f90d6e8
Marina Guimarães <mama_guima04@hotmail.com>
2024-07-11 11:36:28 -0300

    feat: Implementing controllers from trips

---
+++ src/controllers/trips_creator.py
@@ -0,0 +1,30 @@
+from typing import Dict
+import uuid
+
+
+class TripCreator:
+    def __init__(self, trips_repository, email_repository) -> None:
+        self.__trips_repository = trips_repository
+        self.__email_repository = email_repository
+
+    def create(self, body) -> Dict:
+        try:
+            emails = body.get("email_to_invite")
+
+            trip_id = str(uuid.uuid4())
+            trip_infos = {**body, "id": trip_id}
+
+            self.__trips_repository.create_trip(trip_infos)
+
+            if emails:
+                for email in emails:
+                    self.__email_repository.registry_email(
+                        {"email": email, "trip_id": trip_id, "id": str(uuid.uuid4())}
+                    )
+
+            return {"body": {"id": trip_id}, "status_code": 201}
+        except Exception as exception:
+            return {
+                "body": {"error": "Bad Request", "message": str(exception)},
+                "status_code": 400,
+            }
